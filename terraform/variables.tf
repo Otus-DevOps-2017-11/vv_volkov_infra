a4a15c78cb512d2e3fa215ae58c449600ac8e465
Viktor Volkov <megachupakabra007@mail.ru>
2018-01-15 23:19:34 +0300

    first preparations

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Public key path"
+}
+
+variable private_key_path {
+  description = "Private key path"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
